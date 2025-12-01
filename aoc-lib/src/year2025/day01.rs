1ceb7d6df545bd468db58b9350e734aed14da96f
Bart Terpstra <bartterpstra@protonmail.com>
2025-12-01 20:16:36 +0100

    6% improvement by not saving partial count, but collecting immediately

--- aoc-lib/src/year2025/day01.rs
+++ aoc-lib/src/year2025/day01.rs
@@ -3,14 +3,14 @@ use anyhow::Result;
 
 pub fn solve() -> Result<()> {
     let input = utils::load_input(2025, 1)?;
-    let instructions = to_instructions(&input);
+    let (instructions,count) = to_instructions(&input);
 
     let part1 = solve_part1(&instructions)?;
     let part2 = solve_part2(&instructions)?;
 
     println!("Day 1 / Year 2025");
     println!("Part 1: {}", part1);
-    println!("Part 2: {}", part2);
+    println!("Part 2: {}", part2+count as u32);
 
     Ok(())
 }
@@ -28,16 +28,16 @@ fn new()->Machine{
 struct Instruction {
     is_left: bool,
     value:u8,
-    zerocount_bonus:u8,
 }
 
 fn is_left(c:char) ->bool{
     c == 'L'
 }
 
-fn to_instructions(file:&str) ->Vec<Instruction>{
-    let dummy = Instruction{is_left:false, value:0,zerocount_bonus:0};
+fn to_instructions(file:&str) ->(Vec<Instruction>, u16){
+    let dummy = Instruction{is_left:false, value:0};
     let mut instructions: Vec<Instruction>= vec![dummy;1000];
+    let mut bonus_count:u16 = 0;
     for line in file.trim().lines() {
         //assert len 2-3
         //assert format as (L|R)[0-9]{1,3}
@@ -46,11 +46,11 @@ fn to_instructions(file:&str) ->Vec<Instruction>{
         let slice: u16 = (&line[1..]).parse::<u16>().unwrap();
 
         //the idea is to do division and remainder before we store the data, so we save processing and memory.
-        let zerocount_bonus:u8 = (slice / 100) as u8; //999/100 <= 9 <= 256
-        let value:u8 = (slice % 100) as u8; //n%100 <= 100 <= 256
-        instructions.push(Instruction { is_left: is_left(direction),value, zerocount_bonus });
+        bonus_count += slice / 100; //999/100 <= 9 <= 256
+        let value :u8 = (slice % 100) as u8; //n%100 <= 100 <= 256
+        instructions.push(Instruction { is_left: is_left(direction),value });
     }
-    return instructions;
+    return (instructions, bonus_count);
 }
 fn solve_part1(instructions: &Vec<Instruction>) -> Result<u32> {
     let mut machine = new();
@@ -105,9 +105,7 @@ fn solve_part2(instructions: &Vec<Instruction>) -> Result<u32> {
         }
         was_zero = machine.position == 0;
     }
-
-    let bonus =  instructions.iter().map(|x|->u32{x.zerocount_bonus as u32}).sum::<u32>();
-    Ok(machine.zerocount +bonus)
+    Ok(machine.zerocount)
 }
 
 #[cfg(test)]
@@ -127,15 +125,16 @@ L82";
 
     #[test]
     fn test_part1() {
-        let instructions = to_instructions(EXAMPLE);
+        let (instructions,count) = to_instructions(EXAMPLE);
         let result = solve_part1(&instructions).unwrap();
         assert_eq!(result.to_string(), "3");
     }
 
     #[test]
     fn test_part2() {
-        let instructions = to_instructions(EXAMPLE);
+        let (instructions,count) = to_instructions(EXAMPLE);
         let result = solve_part2(&instructions).unwrap();
-        assert_eq!(result.to_string(), "6");
+        let combined = result+count as u32;
+        assert_eq!(combined.to_string(), "6");
     }
 }
