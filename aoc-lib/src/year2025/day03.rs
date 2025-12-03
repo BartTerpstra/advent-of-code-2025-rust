428870f920b0f17cda5617b79fbf7912ad3d6adf
Bart Terpstra <bartterpstra@protonmail.com>
2025-12-03 12:05:31 +0100

    part 1, passes test

--- aoc-lib/src/year2025/day03.rs
+++ aoc-lib/src/year2025/day03.rs
@@ -14,9 +14,57 @@ pub fn solve() -> Result<()> {
     Ok(())
 }
 
+fn as_banks(file: &str) -> Vec<Vec<char>>{
+    //assert 100 digits > u128 == a problem
+    //assert no invalid input
+    let mut results:Vec<Vec<char>> = Vec::with_capacity(200);
+    let lines = file.trim().lines();
+    for x in lines {
+        results.push(digits_of(x));
+    }
+
+    results
+}
 fn solve_part1(_input: &str) -> Result<impl std::fmt::Display> {
-    // TODO: Implement part 1
-    Ok(0)
+
+    let banks = as_banks(_input);
+    let mut sum=0;
+    for bank in banks {
+        //assert highest is never allowed to be the last index (14)
+        //assert indexes are not allowed to be equal
+        let mut index_of_highest = 0;
+        let mut index_of_second_highest = 9999999;
+        for i in 1..bank.len()-1{ //last index is not allowed!
+            //assert the following absolute ordering for chars: 0<1<2<3<4<5<6<7<8<9
+            if bank[index_of_highest] < bank[i]{
+                index_of_highest = i;
+                //small optim
+                if bank[index_of_highest] == '9'{
+                    break;
+                }
+            }
+        }
+        index_of_second_highest = index_of_highest+1;
+
+        for i in index_of_second_highest..bank.len(){
+            if bank[index_of_second_highest] < bank[i]{
+                println!("updated");
+                println!("{:?}", bank);
+                println!("high: {}, curr: {}, new:{}", index_of_highest, index_of_second_highest, i);
+
+                index_of_second_highest = i;
+            }
+        }
+        let tensplace = bank[index_of_highest].to_digit(10).unwrap()*10;
+        let onesplace = bank[index_of_second_highest].to_digit(10).unwrap();
+        println!("value {}",bank[index_of_second_highest]);
+        println!("index {}",index_of_second_highest);
+        println!("interpreted as {}",onesplace);
+        let charge = tensplace+onesplace;
+        println!("charge: {}",charge);
+        sum+=charge;
+    }
+    Ok(sum)
 }
 
 fn solve_part2(_input: &str) -> Result<impl std::fmt::Display> {
@@ -24,16 +72,31 @@ fn solve_part2(_input: &str) -> Result<impl std::fmt::Display> {
     Ok(0)
 }
 
+fn digits_of(mut i:&str)->Vec<char> {
+    let result = i.chars().collect::<Vec<char>>();
+    result
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    const EXAMPLE: &str = "";
+    const EXAMPLE: &str = "987654321111111
+811111111111119
+234234234234278
+818181911112111";
+
+    #[test]
+    fn test_parser() {
+        let result = as_banks(EXAMPLE);
+        println!("{:?}", result);
+        // assert_eq!(result.to_string(), "357");
+    }
 
     #[test]
     fn test_part1() {
         let result = solve_part1(EXAMPLE).unwrap();
-        assert_eq!(result.to_string(), "0");
+        assert_eq!(result.to_string(), "357");
     }
 
     #[test]
