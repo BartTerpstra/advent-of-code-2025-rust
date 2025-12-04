fbccc0f19f2b8b02a83b6c0e6c20695b9bbe96c6
Bart Terpstra <bartterpstra@protonmail.com>
2025-12-04 13:00:12 +0100

    day 4, and everything works first try. very satisfying.

--- aoc-lib/src/year2025/day04.rs
+++ aoc-lib/src/year2025/day04.rs
@@ -48,6 +48,9 @@ fn to_num(boolean:bool)->u8{
 }
 
 fn getAvailable(floor: &Shopfloor, coordinate:Coordinate) -> u8{
+
+   if(to_num(floor[coordinate.height][coordinate.width]) == 0){return 9;}
+
     //assert coordinates are always atleast 1 in from the edge
     let mut sum = 0;
     sum += to_num(floor[coordinate.height-1][coordinate.width-1]);
@@ -63,6 +66,12 @@ fn getAvailable(floor: &Shopfloor, coordinate:Coordinate) -> u8{
     sum += to_num(floor[coordinate.height+1][coordinate.width+1]);
 
     sum
+}
+
+fn moveAll(floor: &mut Shopfloor, coordinates:Vec<Coordinate>) {
+    for coord in coordinates {
+        floor[coord.height][coord.width] = false;
+    }
 }
 fn solve_part1(_input: &str) -> Result<impl std::fmt::Display> {
     let floor  = as_shopfloor(_input);
@@ -82,8 +91,30 @@ fn solve_part1(_input: &str) -> Result<impl std::fmt::Display> {
 }
 
 fn solve_part2(_input: &str) -> Result<impl std::fmt::Display> {
-    // TODO: Implement part 2
-    Ok(0)
+    let mut floor  = as_shopfloor(_input);
+    let mut total = 0;
+    //-1 because we are skipping the far edge
+    let max_height = floor.len()-1;
+    let max_width = floor[0].len()-1;
+    loop {
+        let mut subtotal = 0;
+        let mut coordinates_to_move:Vec<Coordinate> = vec![];
+        for height in 1..max_height {
+            for width in 1..max_width {
+                if getAvailable(&floor, Coordinate { height, width }) < 4 {
+                    coordinates_to_move.push(Coordinate { height, width });
+                    subtotal += 1;
+                }
+            }
+        }
+        moveAll(&mut floor, coordinates_to_move);
+        total+=subtotal;
+        if subtotal == 0{
+            break;
+        }
+    }
+
+    Ok(total)
 }
 
 #[cfg(test)]
@@ -117,6 +148,6 @@ mod tests {
     #[test]
     fn test_part2() {
         let result = solve_part2(EXAMPLE).unwrap();
-        assert_eq!(result.to_string(), "0");
+        assert_eq!(result.to_string(), "43");
     }
 }
