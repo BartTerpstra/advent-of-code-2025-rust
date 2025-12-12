99f8c329e621ff05975b56e8f8b0c7a0131f6007
Bart Terpstra <bartterpstra@protonmail.com>
2025-12-12 20:35:38 +0100

    oh, I can feel it in my bones, I will have lifetimes

--- aoc-lib/src/year2025/day08.rs
+++ aoc-lib/src/year2025/day08.rs
@@ -4,7 +4,7 @@ use anyhow::Result;
 pub fn solve() -> Result<()> {
     let input = utils::load_input(2025, 8)?;
 
-    let part1 = solve_part1(&input)?;
+    let part1 = solve_part1(&input, 1000)?;
     let part2 = solve_part2(&input)?;
 
     println!("Day 8 / Year 2025");
@@ -14,16 +14,68 @@ pub fn solve() -> Result<()> {
     Ok(())
 }
 
-fn solve_part1(_input: &str) -> Result<impl std::fmt::Display> {
+#[derive(Clone)]
+struct Hub {
+    x: i64,
+    y: i64,
+    z: i64,
+    id: u128
+}
+
+struct Link{
+    to:Hub,
+    from:Hub,
+    distance:u64
+}
+
+struct Network{
+    links: Vec<Link>,
+    id:u128
+}
+fn abs_diff(a:i64,b:i64) -> i64{
+    (a-b).abs()
+}
+fn get_distance(a:&Hub, b:&Hub) ->u64{
+    //sum of difference between 3 axis.
+    let x = abs_diff(a.x,b.x);
+    let y = abs_diff(a.y,b.y);
+    let z = abs_diff(a.z,b.z);
+
+    let distance = (x+y+z) as u64;//todo change this
+
+    distance
+}
+
+fn solve_part1(_input: &str, mut connections_to_make:u32) -> Result<impl std::fmt::Display> {
     //really, this is like a naive solution to the 3 dimensional traveling salesman problem
     //combined with dijkstra
 
     //let's have virtual networks.
     //start with a list of points with network IDs equal to their index
     //for every point against every point not checked (n^2)
+    let hubs:Vec<Hub> = vec![];
+
     //calculate distance
     //store in list with ref L, ref R, distance.
     //sort.
+    let mut links:Vec<Link> = vec![];
+    for index in 0..hubs.len(){
+        for inner in index..hubs.len(){
+            let distance = get_distance(
+                &hubs[index],
+                &hubs[inner]
+            );
+            let l = Link{
+                to: (&hubs[index]).clone(),
+                from: hubs[inner].clone(),
+                distance};
+            links.push(l);
+        }
+    }
+
+    let mut networks:Vec<Network> = vec![];
+    while (connections_to_make > 0){
+    }
     //for every item
     //if L.get_network == R.get_network continue
     //else combine_networks(l.get_network, r.get_network)
@@ -62,7 +114,7 @@ mod tests {
 
     #[test]
     fn test_part1() {
-        let result = solve_part1(EXAMPLE).unwrap();
+        let result = solve_part1(EXAMPLE, 40).unwrap();
         assert_eq!(result.to_string(), "40");
     }
 
